40a984fa2509cae39900748272af243dd3590db1
msalves@008 <matheusdesousaalves2018@gmail.com>
2021-09-29 00:47:19 -0300

    create Chart User Analytics

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+  export const userData = [
+    {
+      name: 'Jan',
+      "Active User": 4000,
+    },
+    {
+      name: 'Feb',
+      "Active User": 4090,
+    },
+    {
+      name: 'Mar',
+      "Active User": 6000,
+    },
+    {
+      name: 'Apr',
+      "Active User": 4000,
+    },
+    {
+      name: 'May',
+      "Active User": 5670,
+    },
+    {
+      name: 'Jun',
+      "Active User": 6580,
+    },
+    {
+      name: 'Jul',
+      "Active User": 5000,
+    },
+    {
+      name: 'Agu',
+      "Active User": 7254,
+    },
+    {
+      name: 'Sep',
+      "Active User": 8000,
+    },
+    {
+      name: 'Oct',
+      "Active User": 8500,
+    },
+    {
+      name: 'Nov',
+      "Active User": 8300,
+    },
+    {
+      name: 'Dec',
+      "Active User": 10000,
+    },
+  ];
