e8536becef3930d62f09cf2beb346d0ad7bfc898
YusifNajat <eng.yusif98@gmail.com>
2021-12-18 01:36:30 +0300

    18/12/2021

--- src/components/Topbar/TopBar.jsx
+++ src/components/Topbar/TopBar.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import './topbar.css';
-import {NotificationsNone, Language, Settings} from '@material-ui/icons';
-
+import {NotificationsNone, Language,LocationOn} from '@material-ui/icons';
+import { ProductConsumer } from '../../Context'
 
 export  default function Topbar(){
   return (
+    <ProductConsumer>
+    {(value) => {
+     const { LogOut_User} = value;
+     return (
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
           <span className="logo">Dyotech ADM</span>
         </div>
         <div className="topRigth">
-          <div className="topbarIconsContainer">
+          {/* <div className="topbarIconsContainer">
              <NotificationsNone/>
              <span className="topIconBadge">2</span>
           </div>
           <div className="topbarIconsContainer">
              <Language/>
              <span className="topIconBadge">2</span>
+          </div> */}
+          <div className="topbarIconsContainer" onClick={LogOut_User}>
+             {/* <Settings/>              */} Log Out
           </div>
-          <div className="topbarIconsContainer">
-             <Settings/>             
-          </div>
-          <img src="https://avatars.githubusercontent.com/u/48848963?v=4" alt="" className="topAvatar" />
+          {/* <img src="https://avatars.githubusercontent.com/u/48848963?v=4" alt="" className="topAvatar" /> */}
         </div>
       </div>
     </div>
+       );
+      }}
+     </ProductConsumer>
   );
 }
 
