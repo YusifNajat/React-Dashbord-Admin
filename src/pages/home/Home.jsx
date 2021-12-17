c66028cec890ca301d449b8170dd6a5fa9d64514
YusifNajat <eng.yusif98@gmail.com>
2021-12-18 01:37:04 +0300

    18/12/2021

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,12 +9,12 @@ import WidgetLg from "../../components/widgetLg/WidgetLg"
 export default function Home() {
   return (
     <div className="home">
-      <FeaturedInfo/>
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      {/* <FeaturedInfo/> */}
       <div className="homeWidget">
         <WidgetSm/>
         <WidgetLg/>
       </div>
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
 
     </div>
   )
