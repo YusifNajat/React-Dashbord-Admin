471e4ea2ac432c1f542e16c3bfd270de8dc6ddc1
YusifNajat <eng.yusif98@gmail.com>
2021-12-18 01:37:10 +0300

    18/12/2021

---
+++ src/pages/newUser/Datas.js
@@ -0,0 +1,39 @@
+const Datas = [
+ {
+  Sallahadin: [
+   "Choose",
+   "Engineering",
+   "Science",
+   "Agriculture",
+   "Education",
+   "Languages",
+   "Law",
+   "Basic_Education",
+   "Islamic_Science",
+  ],
+  Knowledge: [
+   "Choose",
+   "Engineering",
+   "Science",
+   "Law",
+   "Education",
+   "Pharmacy",
+  ],
+  Cihan: [
+   "Choose",
+   "Engineering",
+   "Science",
+   "Education",
+   "Health_Technology",
+  ],
+  Tishk: [
+    "Choose",
+   "Engineering",
+   "Science",
+   "Education",
+   "Pharmacy",
+  ],
+ },
+];
+
+export default Datas;
