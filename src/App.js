4dd94bce50fbec0372d1396ea7ae6989a9bc82d9
YusifNajat <eng.yusif98@gmail.com>
2021-12-18 01:28:51 +0300

    18/12/2021

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import './App.css'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom' ;
-
-import Topbar from "./components/Topbar/TopBar";
-import Sidebar from "./components/sidebar/Sidebar"
+import { BrowserRouter as Router, Switch, Route,Redirect} from 'react-router-dom' ;
+// import 'tachyons'
 import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
 import User from './pages/user/User' ;
 import NewUser from './pages/newUser/NewUser';
 import ProductList from './pages/productList/ProductList';
-
+import LoginForm from './Forms/LoginForm';
+import All from './All';
+import Register from './Forms/Register';
+import { ProductConsumer } from '../src/Context'
 function App() {
   return (
+    <ProductConsumer>
+    {value=>{
+      const {Auth,GetUser_Login} = value
+  return(<>
     <Router>
-      <Topbar/>
-        <div className="container">
-
-          <Sidebar/>
-          
+    {Auth === true ? <Redirect from='/' to='/dashboard' />:<Redirect from='/dashboard' to='/' />}
           <Switch>
-            <Route exact={true} path="/">
-              <Home/>           
-            </Route>
-
-            <Route exact={true} path="/users">
-              <UserList/>           
-            </Route>
+          <Route exact={true} path="/" component={LoginForm} />
+          <Route exact={true} path="/sign-up" component={Register} />
+            <All GetUser_Login={GetUser_Login}>
+            <Route exact={true} path="/dashboard" component={Home} />
+            <Route exact={true} path="/users" component={UserList} />
             <Route  path="/user/:userId">
               <User/>           
             </Route>
@@ -32,19 +31,16 @@ function App() {
               <NewUser/>           
             </Route>
 
-            <Route exact={true} path="/products">
-              <ProductList/>           
-            </Route>
+            <Route exact={true} path="/products" component={ProductList} />
             <Route  path="/products/:productId">
               <User/>           
             </Route>
-            <Route  exact path="/products">
-              <NewUser/>           
-            </Route>
+            </All>
           </Switch>
-
-        </div>
     </Router>
+    </>) 
+}}
+  </ProductConsumer>
   );
 }
 
