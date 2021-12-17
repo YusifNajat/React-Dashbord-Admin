78817957bd4d0aaa9f56c42bae496e057e83bfe1
YusifNajat <eng.yusif98@gmail.com>
2021-12-18 01:37:50 +0300

    18/12/2021

---
+++ src/pages/newUser/Department_Data.js
@@ -0,0 +1,16 @@
+const Department_Data = [
+ {
+  Engineering: ["Choose", "Architecture", "SoftWare", "Civil"],
+  Science: ["Choose", "Biology", "Mathematics", "Chemistry"],
+  Education: ["Choose", "Mathematics", "Kurdology", "Physically"],
+  Agriculture: ["Choose", "Animal wealth", "Forest", "Plant Protection"],
+  Languages: ["Choose", "English", "Arabic", "Germany"],
+  Pharmacy: ["Choose", "Pharmacy"],
+  Law: ["Choose","Law"],
+  Basic_Education: ["Choose","Kurdology","Physically","Mathematics"],
+  Islamic_Science: ["Choose"],
+  Health_Technology: ["Choose"],
+ },
+];
+
+export default Department_Data;
